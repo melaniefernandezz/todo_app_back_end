b23e078998477cb5c7853089acfd155b935adeb4
Melanie Fernandez <fmelanie997@gmail.com>
2018-05-12 19:15:58 +0000

    remaining of the conditional logic

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,14 +3,29 @@ class TodoController < ApplicationController
     end
     def show
          todo_id = params[:id]
-# The ... signifies that there is more code in this section, but is not shown in the example for the sake of brevity
+
     
-        if todo_id == '1' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
+        if todo_id == '1' 
             @todo_description = "Make the curriculum"
             @todo_pomodoro_estimate = 4
         elsif todo_id == '2'
             @todo_description = "Buy workshop supplies"
             @todo_pomodoro_estimate = 3
+         elsif todo_id == '3' 
+            @todo_description = "Meet with the volunteer trainers"
+            @todo_pomodoro_estimate = 4
+        elsif todo_id == '4'
+        	@todo_description = "Order food for Saturday and Sunday"
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '5'
+        	@todo_description = "Check pre-work assignments "
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '6'
+        	@todo_description = "Send workshop location to all the students"
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '7'
+        	@todo_description = "Have a great workshop"
+        	@todo_pomodoro_estimate = 2
         end
     end
 end 
